11b8a5c89a2c731dd9fbc88c4e63fe6551efea6d
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-06-11 10:55:59 +0900

    2-9

--- ch2/express_app/routes/hello.js
+++ ch2/express_app/routes/hello.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+const db = [
+  { name: "taro", mail: "taro@yamada" },
+  { name: "hanako", mail: "hanako@flower" },
+  { name: "sachiko", mail: "sachiko@happy" },
+  { name: "jiro", mail: "jiro@change" },
+];
+
 router.get("/", function (req, res, next) {
   const data = {
-    title: "hello",
-    message: "これは、サンプルで追加したページです。",
+    title: "Hello!",
+    message: "データを表示します。",
+    db: db,
   };
   res.render("hello", data);
 });
