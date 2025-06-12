801d5adf236d2fd30ce5dda2a3be766e74b4ed8f
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-06-12 10:39:54 +0900

    2-31

--- ch2/express_app/routes/db.js
+++ ch2/express_app/routes/db.js
@@ -4,4 +4,14 @@ const router = express.Router();
 const ps = require("@prisma/client");
 const prisma = new ps.PrismaClient();
 
+router.get("/", async function (req, res, next) {
+  const users = await prisma.user.findMany();
+  const data = {
+    title: "prisma",
+    message: "Userテーブルのレコード一覧。",
+    data: users,
+  };
+  res.render("db", data);
+});
+
 module.exports = router;
